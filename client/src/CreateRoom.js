f4dd464fcddb2754104496d7acfb39396ba02d53
yaser14141 <yaser14141@gmail.com>
2025-04-14 06:03:13 +0300

    Update CreateRoom.js

--- client/src/CreateRoom.js
+++ client/src/CreateRoom.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { useTheme } from '../context/ThemeContext';
+import { useTheme } from './context/ThemeContext';
 import './CreateRoom.css';
 import { FaUsers, FaClock, FaPalette, FaArrowLeft } from 'react-icons/fa';
 
