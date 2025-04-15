c3488cc176e21327dc6b22251f247876017a3a07
yaser14141 <yaser14141@gmail.com>
2025-04-15 13:17:05 +0300

    Update GameGrid.js

--- client/src/GameGrid.js
+++ client/src/GameGrid.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import SquareCell from './SquareCell';
+import SquareCell from './components/SquareCell';
 import { useGame } from './context/GameContext';
 
 const GameGrid = () => {
